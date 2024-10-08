249b5533a374f8e98dca77c1279282b2cf81dcd4
Huguenel <thomas.huguenel@etu.univ-lyon1.fr>
2024-10-08 20:02:08 +0200

    gitignore

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-
+import torchtext
+#torchtext.disable_torchtext_deprecation_warning()
 
 def load_file(file):
     with open(file, 'r') as f:
         return f.read()
 
 if __name__ == '__main__':
-    liste_textes = load_file('dataset/train.txt')
+    fichier = load_file('dataset/train.txt')
+    liste_lignes = fichier.split('\n')
+    liste_textes, liste_emotions = [], []
+    for ligne in liste_lignes:
+        if ligne:
+            texte, emotion = ligne.split(';')
+            liste_textes.append(texte)
+            liste_emotions.append(emotion)
